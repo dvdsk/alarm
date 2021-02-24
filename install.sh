54a6f44dab860b6cf1b50dde1f6b064a669d0516
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-02-24 01:41:38 +0100

    fixes install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cp alarm.desktop ~/.local/share/applications/
+cp morning.png ~/bin/
+cp target/release/alarm ~/bin/
+
+desktop_file="
+[Desktop Entry]
+Name=Morning
+Exec=${HOME}/bin/start_alarm.sh
+Path=${HOME}/bin/
+Type=Application
+Icon=${HOME}/bin/morning.png
+Comment=Set home wakeup alarm
+Keywords=Alarm;Morning;Wakeup;
+Categories=Utility;"
+
+echo "${desktop_file}" > ~/.local/share/applications/alarm.desktop
+
+echo "./alarm r https://domain:port user passw" > ~/bin/start_alarm.sh 
+echo please edit the example start_alarm.sh file in ~/bin/
